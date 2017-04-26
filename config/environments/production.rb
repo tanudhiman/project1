295c07f189b5bdc38f3212d549b032547f12a71e
tanu <tanudhiman287@gmail.com>
2017-04-26 14:51:32 +0530

    env change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,16 @@ Rails.application.configure do
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
-
+  config.action_mailer.default_url_options = { host: 'fierce-shelf-88454.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => "587",
+    :user_name => "tanusmartdata@gmail.com",
+    :password => "manpreetdhiman@21",
+    :authentication => "plain",
+    :enable_starttls_auto => true
+  }
   if ENV['RAILS_LOG_TO_STDOUT'].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
