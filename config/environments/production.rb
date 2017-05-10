3dd8d138ba1f3c7ac95ac81bd8751324763a3c75
tanu <tanudhiman287@gmail.com>
2017-05-10 11:15:41 +0530

    donee

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
   config.assets.js_compressor = :uglifier
   config.assets.paths << "#{Rails.root}/app/assets/video"
-  config.assets.compile = true
+  config.assets.compile = false
   config.log_level = :debug
   config.log_tags = [:request_id]
   config.action_mailer.perform_caching = false
