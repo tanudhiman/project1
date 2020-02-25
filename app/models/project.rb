412818e04fd83daa0f864bd2749d3bff12176e98
Tanu Dhiman <tanu@Tanus-MacBook-Pro.local>
2020-02-25 21:31:12 +0530

    final modifications

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,15 +2,15 @@
 class Project < ApplicationRecord
   has_many :assigns
   has_many :users, :through => 'assigns'
-  has_attached_file :avatar, :styles =>
-  {
-    :thumb => '200x200#',
-    :small  => '400x400>',
-    :medium => '800x800'
-  }, :default_url => 'public/default_image.png'
+  # has_attached_file :avatar, :styles =>
+  # {
+  #   :thumb => '200x200#',
+  #   :small  => '400x400>',
+  #   :medium => '800x800'
+  # }, :default_url => 'public/default_image.png'
 
-  validates_attachment :avatar, content_type:
-  { content_type: ['image/jpg', 'image/jpeg', 'image/png', 'image/gif'] }
+  # validates_attachment :avatar, content_type:
+  # { content_type: ['image/jpg', 'image/jpeg', 'image/png', 'image/gif'] }
   validates :name, presence: true, length: { minimum: 3, maximum: 50 }
   validates :requirements, presence: true
   validates :domain, presence: true
